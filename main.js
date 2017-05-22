e4d23404075056f4fdc3bbceefea1beb3c0c0c13
WRIGHT, SCOTT <scottwright@northwesternmutual.com>
2017-05-22 09:25:24 -0500

    updates

--- main.js
+++ main.js
@@ -1,6 +1,44 @@
 'use strict';
 
+// for (let i = 0; i < a.length; i++) {
+//     console.log(a[i]);
+// }
+
+// a.forEach(function(current, index){
+//     this[index]+=1;
+// }, a)
+// console.log(a)
+
 const a = [1, 2, 3, 4];
-for (let i = 0; i < a.length; i++) {
-    console.log(a[i]);
+
+//map function
+Array.prototype.map2 = function (callBack){
+    let newArr = [];
+    this.forEach(function(current, index){
+        this[index] = callBack(current);
+    }, newArr)
+    return newArr
 }
+
+console.log(a.map2(function(x){return x+=1}))
+
+//filter function
+Array.prototype.filter2 = function(cb){
+    let newArr=[];
+    this.forEach(function(current){
+        if(cb(current)){
+            newArr.push(current)
+        }
+    }, newArr)
+    return newArr
+}
+
+console.log(a.filter2(function(x){return (x%2==0)}))
+
+//console.log(map(a, function(x){return x+=1}))
+
+
+
+// console.log(a.filter(function(curr, index, a){
+//     return (curr % 2 == 0)
+// }))
